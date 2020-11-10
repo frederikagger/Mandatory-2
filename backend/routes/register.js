e49c57b4b28ab5419b81f305f2c62a0c26c19c86
frederikagger <frederikagger@hotmail.com>
2020-11-10 15:46:22 +0100

    create user implemented

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
+const {registerUser} = require("../databaseManger.js");
 
 router.post("/register", (req, res) => {
-    // TODO: create user
+    const user = {
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email
+    };
+    registerUser(user);
+    return res.sendStatus(200);
 });
 
 module.exports = router;
