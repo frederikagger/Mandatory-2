98afa26e7bb621941af9277dbc874c6b3d626799
frederikagger <frederikagger@hotmail.com>
2020-11-10 15:07:53 +0100

    port is now specified without .env

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-const routes = require("./routes/login");
+const routes = require("./routes/register");
+const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 3000;
 
-app.use(express.static("../frontend"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app
+  .use(express.static("../frontend"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(routes);
 
-app.listen(process.env.PORT, () =>
-  console.log(`app listening on port ${process.env.PORT}!`)
+app.listen(port, () =>
+  console.log(`app listening on port ${port}!`)
 );
