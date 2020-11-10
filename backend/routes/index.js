7a0b86fa7272392234317c1fa3eeab562be81673
frederikagger <frederikagger@hotmail.com>
2020-11-10 18:49:08 +0100

    router fixed for now

---
+++ backend/routes/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+router.use(require("./login"))
+router.use(require("./register"))
+
+// TODO: router virker nu, men er ikke sikker på det er en optimal løsning
+
+module.exports = router;
