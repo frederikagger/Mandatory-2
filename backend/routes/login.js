ea47f6c1a92add218052d45e8d508dd435b59b63
frederikagger <frederikagger@hotmail.com>
2020-11-11 09:25:48 +0100

    todo added on login route

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 
 router.post("/login", (req, res) => {
-    
-    res.send("GET request to the homepage");
+  // TODO: authenticate user
+  res.sendStatus(200);
 });
+
+router.get('/index', (req, res) => {
+  res.send('GET request to the homepage')
+})
 
 module.exports = router;
