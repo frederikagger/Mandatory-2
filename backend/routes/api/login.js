fb2108f9ee086de1ac2b63d39697bb628ca67170
frederikagger <frederikagger@hotmail.com>
2020-11-11 21:21:31 +0100

    route cleand up

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -5,8 +5,4 @@ router.post("/login", (req, res) => {
   res.sendStatus(200);
 });
 
-router.get("/index", (req, res) => {
-  res.send("GET request to the homepage");
-});
-
 module.exports = router;
