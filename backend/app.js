4eb788b054b0bc68fe1854ea466335d6f923df39
frederikagger <frederikagger@hotmail.com>
2020-11-11 21:20:46 +0100

    bug fix - fixed static directory

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes/api");
+const pages = require("./routes/pages/index")
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 3000;
 
 app
-  .use(express.static("./public"))
+  .use(express.static("public"))
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
-  .use(routes);
+  .use(routes)
+  .use(pages);
 
 app.listen(port, () =>
   console.log(`app listening on port ${port}!`)
