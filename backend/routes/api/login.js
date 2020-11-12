2b312a5bc5b581a17ca8cd83c406bda24af180b4
frederikagger <frederikagger@hotmail.com>
2020-11-12 22:26:59 +0100

    removed some comments

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -20,23 +20,4 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
-/* 
-  const result = await query.findOne(async (error, user) => {
-    if (error) {
-      console.log(error);
-      return next(error);
-    }
-    if (user) {
-      if (await bcrypt.compare(password, user.password)) {
-        console.log("Found user: ", user);
-        res.sendStatus(200);
-      } else return next(new Error("The password is incorrect."));
-    }
-  });
-  if (!result) {
-    console.log("User not found");
-    res.status(404).send("No user with the current username exist.");
-  }
-}); */
-
 module.exports = router;
