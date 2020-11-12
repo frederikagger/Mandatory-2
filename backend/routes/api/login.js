9bcf6af5c2af7f71034e0b5be49616b14d1626ba
frederikagger <frederikagger@hotmail.com>
2020-11-12 22:25:00 +0100

    implemented http-errors

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -2,10 +2,25 @@ const router = require("express").Router();
 require("../../db/mongoose.js");
 const User = require("../../models/user");
 const bcrypt = require("bcrypt");
+const createError = require("http-errors");
 
 router.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
   const query = User.where({ username: username });
+  try {
+    const user = await query.findOne();
+    if (!user) {
+      throw createError(400, "No user with the current username exist");
+    }
+    if (await bcrypt.compare(password, user.password)) {
+      res.sendStatus(200);
+    } else throw createError(400, "The password is incorrect");
+  } catch (error) {
+    next(error); // * parsing the error to the error handler
+  }
+});
+
+/* 
   const result = await query.findOne(async (error, user) => {
     if (error) {
       console.log(error);
@@ -22,6 +37,6 @@ router.post("/login", async (req, res, next) => {
     console.log("User not found");
     res.status(404).send("No user with the current username exist.");
   }
-});
+}); */
 
 module.exports = router;
--- backend/routes/api/register.js
+++ backend/routes/api/register.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 require("../../db/mongoose.js");
 const User = require("../../models/user");
 const bcrypt = require("bcrypt");
+const createError = require("http-errors");
 
 router.post("/register", async (req, res, next) => {
-  let { password, username, email } = req.body;
-  console.log(password);
-  password = await bcrypt.hash(password, 8);
-  const user = new User({ password, username, email });
-  console.log(user);
   try {
+    let { password, username, email } = req.body;
+    if (password && username && email) {
+      password = await bcrypt.hash(password, 8);
+    } else throw createError(400, "Missing parameters");
+
+    const user = new User({ password, username, email });
     await user.save();
     console.log(`User ${user.username} was created`);
     res.sendStatus(200);
