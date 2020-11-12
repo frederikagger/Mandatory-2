871341296159a660a4f85088fb0e16d90da54445
frederikagger <frederikagger@hotmail.com>
2020-11-12 21:41:51 +0100

    implemented bcrypt

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -1,24 +1,27 @@
 const router = require("express").Router();
 require("../../db/mongoose.js");
 const User = require("../../models/user");
+const bcrypt = require("bcrypt");
 
 router.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
-  const query = User.where({ username: username, password: password });
-  await query.findOne((error, user) => {
+  const query = User.where({ username: username });
+  const result = await query.findOne(async (error, user) => {
     if (error) {
       console.log(error);
-      next(error);
+      return next(error);
     }
     if (user) {
-      console.log("Found user: ",user);
-      res.sendStatus(200);
-    }
-    else {
-      console.log("User not found");
-      res.sendStatus(404);
+      if (await bcrypt.compare(password, user.password)) {
+        console.log("Found user: ", user);
+        res.sendStatus(200);
+      } else return next(new Error("The password is incorrect."));
     }
   });
+  if (!result) {
+    console.log("User not found");
+    res.status(404).send("No user with the current username exist.");
+  }
 });
 
 module.exports = router;
--- backend/routes/api/register.js
+++ backend/routes/api/register.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
 require("../../db/mongoose.js");
 const User = require("../../models/user");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res, next) => {
-  const user = new User(req.body);
+  let { password, username, email } = req.body;
+  console.log(password);
+  password = await bcrypt.hash(password, 8);
+  const user = new User({ password, username, email });
+  console.log(user);
   try {
-    const ref = await user.save();
-    console.log(ref);
+    await user.save();
+    console.log(`User ${user.username} was created`);
     res.sendStatus(200);
   } catch (error) {
     next(error);
