fd53c64ba7d8e05a1c562009eb75e2da5bc33189
frederikagger <frederikagger@hotmail.com>
2020-11-15 20:35:47 +0100

    comment added

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,10 +1,9 @@
 const chalk = require("chalk");
 
+// * this function is logging which routes is visited
 const logger = async (req, res, next) => {
   console.log(chalk.blue(req.method, req.path));
   next();
 };
 
-module.exports = {
-  logger,
-};
+module.exports = logger;
