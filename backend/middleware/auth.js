f2dd88a8f92f31179870095e240849bcd94ab2fb
frederikagger <frederikagger@hotmail.com>
2020-11-15 20:34:39 +0100

    auth almost implemented

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,24 @@
 const path = require("path");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const createError = require("http-errors");
 
-const auth = (req, res, next) => {
-  console.log("auth middleware running");
-  const loggedIn = false;
-  if (loggedIn) {
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.secret);
+    console.log(decoded);
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+    if (!user) {
+      throw new createError("403", "Authentication failed");
+    }
+    req.user = user;
     next();
-  } else {
-    return res.sendFile(path.resolve("public/html/login.html"));
+  } catch (error) {
+    next(error);
   }
 };
 
