f9dd502894722cc49bc3eec4cbdbb2f7402a297e
frederikagger <frederikagger@hotmail.com>
2020-11-17 09:11:08 +0100

    implemented cookies instead of localstorage

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,24 +1,22 @@
-const path = require("path");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const createError = require("http-errors");
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header("Authorization").replace("Bearer ", "");
+    const token = req.cookies.token;
     const decoded = jwt.verify(token, process.env.secret);
-    console.log(decoded);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
     });
     if (!user) {
-      throw new createError("403", "Authentication failed");
+      throw new createError("401", "Authentication failed");
     }
     req.user = user;
     next();
   } catch (error) {
-    next(error);
+    return res.redirect("/login");
   }
 };
 
