d837b1f5bf35a19738dc240aa868a12a1a9ebe84
HansenKenneth <arme@taxastork.dk>
2020-11-17 16:57:07 +0100

    updated contact and added css file

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 router.use(require("./login"));
 router.use(require("./register"));
 router.use(require("./user"));
+router.use(require("./contact"));
 
 // TODO: router virker nu, men er ikke sikker på det er en optimal løsning
 
