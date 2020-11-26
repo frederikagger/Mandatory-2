3c002d0b2416cf50045681632f23cb908280fc6c
HansenKenneth <arme@taxastork.dk>
2020-11-26 13:41:23 +0100

    bugfix middelware/auth SECRET

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const createError = require("http-errors");
 const auth = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-    const decoded = jwt.verify(token, process.env.secret);
+    const decoded = jwt.verify(token, process.env.SECRET);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
